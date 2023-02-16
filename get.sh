9ad775f165d33bcde3b1b41498bc10e9d104b5b7
Eshnek <dylanpozarnsky@gmail.com>
2023-02-16 04:56:31 -0700

    Fix

--- get.sh
+++ get.sh
@@ -3,13 +3,13 @@
 sudo mkdir -p roads
 sudo mkdir -p planets
 
-get() {
+wall() {
   if [[ ! -f $1 ]]; then
-    sudo get -O $1 $2
+    sudo wget -O $1 $2
   fi
 }
 
-get roads/valley-of-fire-road-nevada.jpg https://4kwallpapers.com/images/wallpapers/valley-of-fire-state-park-road-tarmac-highway-nevada-3648x5472-1096.jpg
+wall roads/valley-of-fire-road-nevada.jpg https://4kwallpapers.com/images/wallpapers/valley-of-fire-state-park-road-tarmac-highway-nevada-3648x5472-1096.jpg
 
-get planets/saturn_a.jpg https://4kwallpapers.com/images/wallpapers/saturn-solar-system-5822x3824-10203.jpg
+wall planets/saturn_a.jpg https://4kwallpapers.com/images/wallpapers/saturn-solar-system-5822x3824-10203.jpg
 

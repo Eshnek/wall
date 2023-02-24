f12f2faecec953b2aee1210ca126c89eac0c1ead
Eshnek <dylanpozarnsky@gmail.com>
2023-02-24 08:30:39 -0700

    Space

--- get.sh
+++ get.sh
@@ -3,6 +3,7 @@
 sudo mkdir -p roads
 sudo mkdir -p planets
 sudo mkdir -p scapes
+sudo mkdir -p space
 
 wall() {
   if [[ ! -f $1 ]]; then
@@ -16,3 +17,5 @@ wall planets/saturn_a.jpg https://4kwallpapers.com/images/wallpapers/saturn-sola
 
 wall scapes/sea_a.jpg https://4kwallpapers.com/images/wallpapers/seascape-sunset-rocks-twilight-summer-5k-8k-7600x5069-8538.jpg
 wall scapes/mountains_a.jpg https://4kwallpapers.com/images/wallpapers/dolomite-mountains-summer-italian-alps-sunset-mountain-view-7680x4833-8574.jpg
+
+wall space/space_a.jpg https://4kwallpapers.com/images/wallpapers/stars-galaxy-3840x2560-10307.jpg
